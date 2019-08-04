08537b02b0179070025c58872cb49e71915a7d24
yuanmeng <473127362@qq.com>
2019-08-04 19:05:46 +0800

    测试上传项目222

--- src/main/java/com/baidu/dao/KcInfoDao.java
+++ src/main/java/com/baidu/dao/KcInfoDao.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Created by Dell on 2019/7/26.
  */
 public interface KcInfoDao extends JpaRepository<Kc,Long>{
-    //111
+    //2222
+//    发的发的发的
 }
